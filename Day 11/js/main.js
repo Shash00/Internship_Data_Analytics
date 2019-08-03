a7c6a17140d4ce5df89ba685d4de6736baabab0e
Shashank K Bhat <shashank.kakunje@gmail.com>
2019-08-03 15:36:53 +0530

    FMS

--- Day 11/js/main.js
+++ Day 11/js/main.js
@@ -21,10 +21,16 @@ function showResult(searchStr){
 
 function showTable(data){
     str = "<table class = 'table'>";
+    if(!data[0]){
+        str += <center><h1>Not found</h1></center>
+    }
     str += "<tr><th>Name</th><th>Qualification</th><th>Department</th><th>Status</th></tr>";
 
     for(i=0;i<data.length;i++){
-
+        
+        if(data[i]["qualification"] == ""){
+            data[i]["qualification"] = "<small>Not found</small>"
+        }
         str += "<tr>";
         str += "<td>"+data[i]["name"]+"</td>"
         str += "<td>"+data[i]["qualification"]+"</td>"
@@ -36,3 +42,13 @@ function showTable(data){
     str += "</table>"
     $("#idTable").html(str);
 }
+
+$("#idViewAll").click(function(){
+    $.ajax({
+        url: "https://fms-mite.herokuapp.com/fms/",
+        method : "GET",
+        success : function(data){
+            showTable(data);
+        }
+    })
+})
