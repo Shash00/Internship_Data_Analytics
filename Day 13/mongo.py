9dcdb1e8a36f1564511fdca27ef21ba025f11856
Shashank K Bhat <shashank.kakunje@gmail.com>
2019-08-06 13:00:52 +0530

    Day 13 Mongo

---
+++ Day 13/mongo.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import pymongo as m
+
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client["test"]
+
+collection = db["faculty"]
+
+query = {}
+project = {"name":1, "salary":1, "_id":0}
+for doc in collection.find(query).sort([("name", m.ASCENDING)]):
+    print(doc["name"])
+ 
